a111cfcbb288daf126c0956eaecb5c71e4057123
upiksaleh <upxsal@gmail.com>
2024-02-04 22:10:20 +0800

    update dockerswarm (add disks)

--- terraform/prod/.terraform.lock.hcl
+++ terraform/prod/.terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/local" {
   version = "2.4.1"
   hashes = [
     "h1:FzraUapGrJoH3ZOWiUT2m6QpZAD+HmU+JmqZgM4/o2Y=",
+    "h1:gpp25uNkYJYzJVnkyRr7RIBVfwLs9GSq2HNnFpTRBg0=",
     "zh:244b445bf34ddbd167731cc6c6b95bbed231dc4493f8cc34bd6850cfe1f78528",
     "zh:3c330bdb626123228a0d1b1daa6c741b4d5d484ab1c7ae5d2f48d4c9885cc5e9",
     "zh:5ff5f9b791ddd7557e815449173f2db38d338e674d2d91800ac6e6d808de1d1d",
@@ -25,6 +26,7 @@ provider "registry.terraform.io/vmware/vcd" {
   constraints = "3.11.0"
   hashes = [
     "h1:7cbQUrQ3k+Gu67N9MSsR91DIJKj4LtbzkL2X0Iv7H4U=",
+    "h1:dSBcQbq9Zku9hFJ/AHbPVdCqea4m4UwVBqbC3DbTWoo=",
     "h1:fz++5WjF65aD01TfzgEC8Kjg3+p8RjPjh4koU/wj6ic=",
     "zh:1066cc44938ecd24a1cd199a83dbe2f5a739c89ea728451a85c2eb91ed7c393f",
     "zh:13f35b5e64075d470865d2b9a0c887a8d6cd272bc1f13ccf75046c52fd5c2ac4",
--- terraform/prod/vapp-dockerswarm.tf
+++ terraform/prod/vapp-dockerswarm.tf
@@ -17,22 +17,39 @@ resource "vcd_vapp_org_network" "dockerswarm_wanInet" {
   reboot_vapp_on_removal = true
 }
 
+resource "vcd_independent_disk" "dockerswarm_disk_master" {
+  name         = "dockerswarm_disk_master"
+  size_in_mb   = 32 * 1024
+  bus_type     = "SCSI"
+  bus_sub_type = "VirtualSCSI"
+
+}
+
 module "vms_dockerswarm" {
+  source = "../modules/vcd-vapp-vm-ubuntucloud"
   for_each = {
     "dswarm-master" = {
-      cpus   = 2
-      memory = 2
+      disks = [{
+        name        = vcd_independent_disk.dockerswarm_disk_master.name
+        bus_number  = 0
+        unit_number = 1
+      }]
     }
     "dswarm-node1" = {}
     "dswarm-node2" = {}
+    "dswarm-node3" = {}
+    "dswarm-node4" = {}
   }
-  source = "../modules/vcd-vapp-vm-ubuntucloud"
 
-  vapp_name = vcd_vapp.dockerswarm.name
-  name      = each.key
-  hostname  = each.key
-  cpus      = can(each.value.cpus) ? each.value.cpus : 4
-  memory    = (can(each.value.memory) ? each.value.memory : 8) * 1024
+  vapp_name     = vcd_vapp.dockerswarm.name
+  name          = replace(each.key, "dswarm-", "")
+  hostname      = each.key
+  computer_name = each.key
+  cpus          = can(each.value.cpus) ? each.value.cpus : 8
+  memory        = (can(each.value.memory) ? each.value.memory : 16) * 1024
+
+  template_disk_size = 64 * 1024
+  disks              = lookup(each.value, "disks", [])
 
   local_admin_password       = var.local_admin_password
   local_admin_authorized_key = var.local_admin_authorized_key
@@ -40,12 +57,12 @@ module "vms_dockerswarm" {
 
   networks = [
     {
-      name       = vcd_vapp_org_network.dockerswarm_lanMgmt.org_network_name
-      is_primary = false
-    },
-    {
       name       = vcd_vapp_org_network.dockerswarm_wanInet.org_network_name
       is_primary = true
+    },
+    {
+      name       = vcd_vapp_org_network.dockerswarm_lanMgmt.org_network_name
+      is_primary = false
     }
   ]
 }
