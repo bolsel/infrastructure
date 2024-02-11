edd4466d70b2befeb938aeaf0c3acc03b5fe0b53
Upik Saleh <upxsal@gmail.com>
2024-02-11 19:57:45 +0800

    add vcd docker-swarm

---
+++ terraform/vcd/docker-swarm/.terraform.lock.hcl
@@ -0,0 +1,44 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.4.1"
+  constraints = "2.4.1"
+  hashes = [
+    "h1:FzraUapGrJoH3ZOWiUT2m6QpZAD+HmU+JmqZgM4/o2Y=",
+    "zh:244b445bf34ddbd167731cc6c6b95bbed231dc4493f8cc34bd6850cfe1f78528",
+    "zh:3c330bdb626123228a0d1b1daa6c741b4d5d484ab1c7ae5d2f48d4c9885cc5e9",
+    "zh:5ff5f9b791ddd7557e815449173f2db38d338e674d2d91800ac6e6d808de1d1d",
+    "zh:70206147104f4bf26ae67d730c995772f85bf23e28c2c2e7612c74f4dae3c46f",
+    "zh:75029676993accd6bef933c196b2fad51a9ec8a69a847dbbe96ec8ebf7926cdc",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7d48d5999fe1fcdae9295a7c3448ac1541f5a24c474bd82df6d4fa3732483f2b",
+    "zh:b766b38b027f0f84028244d1c2f990431a37d4fc3ac645962924554016507e77",
+    "zh:bfc7ad301dada204cf51c59d8bd6a9a87de5fddb42190b4d6ba157d6e08a1f10",
+    "zh:c902b527702a8c5e2c25a6637d07bbb1690cb6c1e63917a5f6dc460efd18d43f",
+    "zh:d68ae0e1070cf429c46586bc87580c3ed113f76241da2b6e4f1a8348126b3c46",
+    "zh:f4903fd89f7c92a346ae9e666c2d0b6884c4474ae109e9b4bd15e7efaa4bfc29",
+  ]
+}
+
+provider "registry.terraform.io/vmware/vcd" {
+  version     = "3.11.0"
+  constraints = "3.11.0"
+  hashes = [
+    "h1:fz++5WjF65aD01TfzgEC8Kjg3+p8RjPjh4koU/wj6ic=",
+    "zh:1066cc44938ecd24a1cd199a83dbe2f5a739c89ea728451a85c2eb91ed7c393f",
+    "zh:13f35b5e64075d470865d2b9a0c887a8d6cd272bc1f13ccf75046c52fd5c2ac4",
+    "zh:3f8593c96171801f9ad6199ce8bec3fc47455c17d72353489a68f0e9f6dce70e",
+    "zh:44ecb85c2c4eb3626c75e7afdcd26b776c679ac63ec5ead6b5a33fed97d4c898",
+    "zh:63e00e75f354bdb4e543b717ca8299fb4216420324e93b979c87b6841ff84ceb",
+    "zh:9a88020d8136d4e89dd0163e46eef197abe10ba966f1f1b7aa3fc9b0de8d7973",
+    "zh:b995d1929b815c4124d2ff067ae99e4a9668f96679e113d338fa2569d433bec1",
+    "zh:c083c84ee8cd0ce9a51df267ee8b1e4467ef33548870ad0c6714f6cf6f46c031",
+    "zh:e5a42de1551569d1789774319ca8dde31c0c0dd1036845eff181728449989c06",
+    "zh:e6f0d523be198783b8d5073cd41fa63b42418bd4b53a1e6fc483688b9b6804c9",
+    "zh:e81e902c0d11dea8716cb2f6daa71b8275c0ca03325602d3dfbd99c4d41c9fc7",
+    "zh:ea76009ecead6344bf877b39118872b91918751a3c56ffe00162d345759b40f6",
+    "zh:ef834cbee445be3bbb01ffbc494ebf1dd8e5e3178dec394278ca9f16f569536c",
+    "zh:f7706ba2decff84bd53219d65a01663e478c9217539a9d76be60cb784908c420",
+  ]
+}
---
+++ terraform/vcd/docker-swarm/main.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_providers {
+    vcd = {
+      source  = "vmware/vcd"
+      version = "3.11.0"
+    }
+  }
+}
+provider "vcd" {
+  user                 = var.vcd_user
+  password             = var.vcd_pass
+  auth_type            = "integrated"
+  org                  = var.vcd_org
+  vdc                  = var.vcd_vdc
+  url                  = var.vcd_url
+  max_retry_timeout    = var.vcd_max_retry_timeout
+  allow_unverified_ssl = var.vcd_allow_unverified_ssl
+}
+
+//=============================================
+
+data "vcd_network_direct" "wan_inet" {
+  name = var.network_name_wan_inet
+}
+data "vcd_network_direct" "lan_mgmt" {
+  name = var.network_name_lan_mgmt
+}
+
+//=============================================
+
+resource "vcd_vapp" "dswarm" {
+  name        = "Docker Swarm"
+  description = "Docker Swarm Cluster"
+  power_on    = true
+}
+
+resource "vcd_vapp_org_network" "dswarm_lanMgmt" {
+  vapp_name              = vcd_vapp.dswarm.name
+  org_network_name       = data.vcd_network_direct.lan_mgmt.name
+  reboot_vapp_on_removal = true
+}
+
+resource "vcd_vapp_org_network" "dswarm_wanInet" {
+  vapp_name              = vcd_vapp.dswarm.name
+  org_network_name       = data.vcd_network_direct.wan_inet.name
+  reboot_vapp_on_removal = true
+}
+
+resource "vcd_independent_disk" "dswarm_master" {
+  name         = "dswarm-master"
+  size_in_mb   = 32 * 1024
+  bus_type     = "SCSI"
+  bus_sub_type = "VirtualSCSI"
+}
+
+locals {
+  vms = {
+    "master" = {
+      disks = [{
+        name        = vcd_independent_disk.dswarm_master.name
+        bus_number  = 0
+        unit_number = 1
+      }]
+    }
+    "node1" = {}
+    "node2" = {}
+  }
+}
+module "vms_dswarm" {
+  source        = "../../modules/vcd-vapp-vm-ubuntucloud"
+  for_each      = local.vms
+  vapp_name     = vcd_vapp.dswarm.name
+  name          = each.key
+  hostname      = each.key
+  computer_name = each.key
+  cpus          = can(each.value.cpus) ? each.value.cpus : 4
+  memory        = (can(each.value.memory) ? each.value.memory : 8) * 1024
+
+  template_disk_size = 64 * 1024
+  disks              = lookup(each.value, "disks", [])
+
+  local_admin_password       = var.local_admin_password
+  local_admin_authorized_key = var.local_admin_authorized_key
+  automation_authorized_key  = var.automation_authorized_key
+
+  networks = [
+    {
+      name       = vcd_vapp_org_network.dswarm_wanInet.org_network_name
+      is_primary = true
+    },
+    {
+      name       = vcd_vapp_org_network.dswarm_lanMgmt.org_network_name
+      is_primary = false
+    }
+  ]
+}
+
+module "post_apply_dswarm" {
+  source = "../../modules/post-apply"
+  id     = "dswarm"
+  vms = {
+    for k, bd in module.vms_dswarm : k => bd.data
+  }
+  networks_config = {
+    "${data.vcd_network_direct.lan_mgmt.name}" = {
+      if_name : "mgmt"
+    }
+    "${data.vcd_network_direct.wan_inet.name}" = {
+      if_name : "inet"
+    }
+  }
+}
+
+output "name" {
+  value = module.post_apply_dswarm.data
+}
