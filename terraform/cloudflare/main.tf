452cfa8013bfd10f73b1dfab27ff50c674244607
Upik Saleh <upxsal@gmail.com>
2024-03-19 21:01:49 +0800

    cloudflare tf use module init

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -9,23 +9,32 @@ terraform {
   }
 }
 
+//=============================================
+
+module "init" {
+  source   = "../modules/_initialize"
+  cloud_id = "vcd"
+}
+
+//=============================================
+
 provider "cloudflare" {
-  api_token = var.cloudflare_token
+  api_token = module.init.cloudflared.cloudflare_token
 }
 
 data "cloudflare_tunnel" "main" {
-  account_id = var.cloudflare_account_id
-  name       = var.cloudflare_tunnel_name
+  account_id = module.init.cloudflared.cloudflare_account_id
+  name       = module.init.cloudflared.cloudflare_tunnel_name
 }
 
 locals {
-  tunnel_ingress_rules = yamldecode(templatefile("${path.module}/config.yaml", {
-    zone = var.cloudflare_zone
+  tunnel_ingress_rules = yamldecode(templatefile("${module.init.cloudflared.cloudflared_tunnels_file}", {
+    zone = module.init.cloudflared.cloudflare_zone
   }))
 }
 
 resource "cloudflare_tunnel_config" "main" {
-  account_id = var.cloudflare_account_id
+  account_id = module.init.cloudflared.cloudflare_account_id
   tunnel_id  = data.cloudflare_tunnel.main.id
 
   config {
@@ -35,7 +44,7 @@ resource "cloudflare_tunnel_config" "main" {
     dynamic "ingress_rule" {
       for_each = local.tunnel_ingress_rules
       content {
-        hostname = ingress_rule.key == "@" ? var.cloudflare_zone : "${ingress_rule.key}.${var.cloudflare_zone}"
+        hostname = ingress_rule.key == "@" ? module.init.cloudflared.cloudflare_zone : "${ingress_rule.key}.${module.init.cloudflared.cloudflare_zone}"
         service  = ingress_rule.value.service
         dynamic "origin_request" {
           for_each = can(ingress_rule.value.origin_request) ? [ingress_rule.value.origin_request] : []
@@ -56,7 +65,7 @@ resource "cloudflare_tunnel_config" "main" {
 resource "cloudflare_record" "tunnel_ingress_rules" {
   for_each = local.tunnel_ingress_rules
 
-  zone_id = var.cloudflare_zone_id
+  zone_id = module.init.cloudflared.cloudflare_zone_id
   name    = each.key
   type    = "CNAME"
   value   = "${data.cloudflare_tunnel.main.id}.cfargotunnel.com"
--- terraform/modules/_initialize/main.tf
+++ terraform/modules/_initialize/main.tf
@@ -1,6 +1,9 @@
 locals {
   private_dir = "${path.module}/../../../.private"
   cloud       = yamldecode(file("${local.private_dir}/variables/cloud-config/${var.cloud_id}.yml"))
+  cloudflared = merge(yamldecode(file("${local.private_dir}/cloudflared/variables.yml")), {
+    cloudflared_tunnels_file = "${local.private_dir}/cloudflared/tunnels.yml"
+  })
 }
 
 output "cloud_id" {
@@ -9,4 +12,8 @@ output "cloud_id" {
 
 output "cloud" {
   value = local.cloud
+}
+
+output "cloudflared" {
+  value = local.cloudflared
 }
